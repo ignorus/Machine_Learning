44bd08f313f60ac7456863a80bc250acdf5600dc
Tiago Bugan <tiago.debs@gmail.com>
2019-04-10 16:16:00 -0300

    aprendendo predict e fit

---
+++ Aula-1/classificacao.py
@@ -0,0 +1,21 @@
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+misterioso = [1, 1, 1]
+
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+modelo.fit(dados,marcacoes)
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+teste = [misterioso1,misterioso2]
+print(modelo.predict(teste))
